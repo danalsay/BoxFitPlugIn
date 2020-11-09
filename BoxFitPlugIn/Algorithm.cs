c5982f9dd9f30e91a5414b8ecab7d2241a7898d2
danalsay <danalsay@gmail.com>
2020-11-09 09:24:38 -0800

    Add files via upload

---
+++ BoxFitPlugIn/Algorithm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BoxFitPlugIn
+{
+    /// <summary>
+    /// Binary Tree Bin Packing
+    /// </summary>
+    class Algorithm
+    {
+        public ProductBox[] Blocks;
+        public Node Root = new Node();
+
+        public Algorithm(ProductBox[] PBs, double  xBase, double  yBase, double wd, double  ht)
+        {
+            Blocks = PBs;
+            Root.x = xBase;
+            Root.y = yBase;
+            Root.h = ht;
+            Root.w = wd;
+            Root = new Node {w = wd, h = ht };
+        }
+        
+        public void SortBoxesBySize()
+        {
+            this.Blocks.ToList().Sort((a, b) => a.xUnitWidth.CompareTo(b.xUnitWidth) ) ;
+        }
+               
+        public void Fit(ProductBox[] Boxes)
+        {
+            for (int i = Boxes.Length-1; i >= 0; i--)
+                {
+                var node = FindNode(Root, Boxes[i].xUnitWidth, Boxes[i].yUnitHeight);
+                if (node != null)
+                {
+                    var newNode = SplitNode(node, Boxes[i].xUnitWidth, Boxes[i].yUnitHeight);
+                    Boxes[i].BasePoint.X = (int)newNode.x;
+                    Boxes[i].BasePoint.Y = (int)newNode.y;
+                }
+
+            }
+           
+        }
+
+        private Node FindNode(Node rootNode, double boxWidth, double boxLength)
+        {
+            if (rootNode.used)
+            {
+                var nextNode = FindNode(rootNode.RightNode, boxWidth, boxLength);
+
+                if (nextNode == null)
+                {
+                    nextNode = FindNode(rootNode.BottomNode, boxWidth, boxLength);
+                }
+
+                return nextNode;
+            }
+            else if (boxWidth <= rootNode.w && boxLength <= rootNode.h)
+            {
+                return rootNode;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private Node SplitNode(Node node, double boxWidth, double boxLength)
+        {
+            node.used = true;
+            node.BottomNode = new Node {  y = node.y + boxLength, x = node.x, h = node.h - boxLength, w = node.w };
+            node.RightNode = new Node { y = node.y, x = node.x + boxWidth, h = boxLength, w = node.w - boxWidth };
+            return node;
+        }
+
+        internal class Node
+        {
+            public Node RightNode;
+            public Node BottomNode;
+            public double x;
+            public double  y;
+            public double  h;
+            public double  w;
+            public bool used;
+        }
+
+    }
+}
---
+++ BoxFitPlugIn/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoxFitPlugIn
+{
+    public partial class Form1 : Form
+    {  
+        int pnlWidth = 100;
+        int pnlHeight = 600;
+        Panel myContainer = new Panel();
+        ProductBox[] Boxes = {
+            new ProductBox(new Point[] { new Point(10,0), new Point(20,0), new Point(20,10),new Point(30,10),new Point(30,20),new Point(10,20),new Point(10,30),new Point(0,30), new Point(0,10), new Point(10,10) }),
+            new ProductBox(new Point[] { new Point(10,0), new Point(20,0), new Point(20,20),new Point(30,20),new Point(30,30),new Point(0,30),new Point(0,20), new Point(10,20) }),
+            new ProductBox(new Point[] { new Point(0,0), new Point(20,0), new Point(20,30),new Point(0,30),new Point(0,20),new Point(10,20),new Point(10,10),new Point(0,10) }),
+            new ProductBox(new Point[] { new Point(0,0), new Point(0,10), new Point(20,10),new Point(20,20),new Point(20,30), new Point(10, 30), new Point(10,40),new Point(0,40), }),
+            new ProductBox(new Point[] { new Point(0,0), new Point(20,0), new Point(20,40),new Point(10,40),new Point(10,10),new Point(0,10) }),
+            new ProductBox(new Point[] { new Point(0,0), new Point(30,0), new Point(30,20),new Point(10,20),new Point(10,10),new Point(0,10) }),
+            new ProductBox(new Point[] { new Point(20,0), new Point(30,0), new Point(30, 20), new Point(20,20),new Point(20,30),new Point(0,30),new Point(0,20),new Point(10,20),new Point(10,10),new Point(20,10) }),
+            new ProductBox(new Point[] { new Point(10,0), new Point(20,0), new Point(20,10),new Point(30,10),new Point(30,20),new Point(20,20),new Point(20,30),new Point(10,30),new Point(10,20),new Point(0,20),new Point(0,10),new Point(10,10) }),
+            new ProductBox(new Point[] { new Point(10,0), new Point(30,0), new Point(30,10),new Point(20,10),new Point(20,30),new Point(0,30),new Point(0,20),new Point(10,20) }),
+            new ProductBox(new Point[] { new Point(20,0), new Point(30,0), new Point(30,30),new Point(0,30),new Point(0,20),new Point(20,20) }),
+            new ProductBox(new Point[] { new Point(0,0), new Point(10,0), new Point(10,50),new Point(0,50) }),
+            new ProductBox(new Point[] { new Point(10,0), new Point(20,0), new Point(20,30),new Point(10,30),new Point(10,40),new Point(0,40),new Point(0,20),new Point(10,20) })
+        };
+
+        public Form1()
+        {
+            InitializeComponent();   
+
+        }
+
+        private void DrawProdBox(ProductBox ProdpBox)
+        {
+            Graphics g = myContainer.CreateGraphics();
+            Pen penpolygon = new Pen(Color.Black, 1);
+            for (int i = 0; i < ProdpBox.ArrayPoints.Length; i++)
+            {
+                ProdpBox.ArrayPoints[i].X += ProdpBox.BasePoint.X;
+                ProdpBox.ArrayPoints[i].Y += ProdpBox.BasePoint.Y;
+            }
+
+            g.DrawPolygon(penpolygon, ProdpBox.ArrayPoints); 
+
+        }
+
+        private void AddContainer( int width, int height)
+        {
+            myContainer.Width = width;
+            myContainer.Height = height;
+            myContainer.BorderStyle = BorderStyle.FixedSingle;
+            myContainer.BackColor = Color.AntiqueWhite;
+            this.Controls.Add(myContainer);
+            myContainer.Location = new System.Drawing.Point(20, 120);
+
+        }
+       
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            { pnlWidth = Convert.ToInt32(txtWidth.Text); }
+            catch { }
+            try { pnlHeight = Convert.ToInt32(txtHeight.Text); }
+            catch { }
+       
+            AddContainer(pnlWidth , pnlHeight);
+            StackBoxes();      
+        }
+
+        private void StackBoxes()
+        {
+            Algorithm A = new Algorithm(Boxes, 0, 0, pnlWidth, pnlHeight);
+            Array.Sort(A.Blocks);
+            A.Fit(A.Blocks);
+            for (int i = 0; i < A.Blocks.Length; i++)
+            {
+                DrawProdBox(A.Blocks[i]);
+            }
+
+        }
+
+        private void LoadBox(int x, int y, System.Drawing.Bitmap bm)
+        {
+            PictureBox pic = new PictureBox();
+            pic.Size = new System.Drawing.Size(20, 20);
+            pic.Location = new System.Drawing.Point(x, y);
+            myContainer.Controls.Add(pic);
+        }
+
+    }
+}
